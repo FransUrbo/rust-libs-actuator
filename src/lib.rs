9cb15d7b35ca22f2bd0db0657c17521fc169cbd7
Turbo Fredriksson <turbo@bayour.com>
2024-06-21 21:48:07 +0100

    Rewrite the actuator controller with actual values.
    We need to use TIME, instead of DISTANCE.
    We know how far it moves from end to end, and we know the resistance
    of those points AND we know how long it takes to move.
    From that, we can calculate how long to enable the actuator to move
    a certain distance.
    
    Calculate the current gear the actuator is in, based on the resistance
    of the potentiometer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,79 @@
 #![no_std]
 
-use defmt::{info, trace};
+use defmt::{error, info};
 
+use embassy_rp::adc;
+use embassy_rp::adc::{Adc, Async as AdcAsync};
 use embassy_rp::gpio::Output;
 use embassy_time::Timer;
 
-// Set the distance between the different mode. 70mm is the total throw from begining to end.
-pub static ACTUATOR_DISTANCE_BETWEEN_POSITIONS: i8 = 70 / 3; // 3 because the start doesn't count :).
+// =====
+// Setup static values, measured on the actuator.
 
-// Actually move the actuator.
-// TODO: How do we actually move the actuator?
-//       Is it enough to send it +3V3 or +5V on the motor relay, or does it need more power? If so,
-//       we need two more MOSFETs.
+// The resistance on the actuator at MIN and MAX throw is 550Ω and 4690Ω respectively.
+static RESISTANCE_MIN_THROW: i16 = 550;
+static RESISTANCE_MAX_THROW: i16 = 4690;
+
+// The total throw from MIN to MAX is `100.60mm` (`3.9605"`).
+static TOTAL_THROW_DISTANCE: f32 = 100.60;
+
+// Moving the actuator between endpoints takes exactly (!) 7s@4-5V; 2s@12V.
+static TOTAL_THROW_TIME: u8 = 2;
+
+// The distance between the different gears is 70mm.
+static DISTANCE_BETWEEN_POSITIONS: u8 = 70;
+
+// The allowed +/- value between MIN and MAX gear position is 2mm.
+static ALLOWED_GEAR_DIFFERENCE: u8 = 2;
+
+// =====
+// These are the actual information we're interested in.
+static TOTAL_THROW_RESISTANCE: i16 = RESISTANCE_MAX_THROW - RESISTANCE_MIN_THROW;
+static THROW_RESISTANCE_PER_1MM: i16 = TOTAL_THROW_RESISTANCE / TOTAL_THROW_DISTANCE as i16;
+static POSITION_MATCH_RANGE: i16 = THROW_RESISTANCE_PER_1MM * ALLOWED_GEAR_DIFFERENCE as i16;
+static THROW_TIME_PER_1MM: f32 = TOTAL_THROW_TIME as f32 / TOTAL_THROW_DISTANCE as f32;
+
+static RESISTANCE_BETWEEN_POSITIONS: i16 =
+    THROW_RESISTANCE_PER_1MM * DISTANCE_BETWEEN_POSITIONS as i16;
+static TIME_DISTANCE_PER_GEAR: u64 =
+    (THROW_TIME_PER_1MM * DISTANCE_BETWEEN_POSITIONS as f32) as u64;
+
+// =====
+
+// Move the actuator.
 pub async fn move_actuator(
-    amount: i8, // Distance in mm in either direction.
+    gears: i8, // Number of gear position(s) in either direction.
     pin_motor_plus: &mut Output<'static>,
     pin_motor_minus: &mut Output<'static>,
+    pot_adc: &mut Adc<'static, AdcAsync>,
+    pot_pin: &mut adc::Channel<'static>,
 ) {
-    if amount < 0 {
-        info!("Moving actuator:  direction=FORWARD; amount={}", amount);
-        pin_motor_minus.set_low(); // Set the other pin to low. There can be only one!
+    // Set both pins to LOW to brake the motor in the actuator.
+    // NOTE: The Arduino example say to set them HIGH, but that will turn ON the relays! (??)
+    // https://www.progressiveautomations.com/blogs/how-to/how-to-use-relays-to-control-linear-actuators
+    pin_motor_plus.set_low();
+    pin_motor_minus.set_low();
 
-        // FAKE: Simulate move by toggling the pin HIGH and LOW `amount` (mm) times.
-        let mut pos: i8 = 0; // Make sure to blink BOTH at completion of every position move.
-        for i in amount..=0 {
-            // FAKE: For every position, turn BOTH led on for a bit, to indicate position.
-            trace!("pos={}; i={}", pos, i);
-            if i % ACTUATOR_DISTANCE_BETWEEN_POSITIONS == 0 {
-                if pos != 0 {
-                    trace!("i % {}", ACTUATOR_DISTANCE_BETWEEN_POSITIONS);
-                    pin_motor_minus.set_high();
-                    pin_motor_plus.set_high();
-                    Timer::after_millis(100).await;
-                    pin_motor_minus.set_low();
-                    pin_motor_plus.set_low();
-                }
+    if gears < 0 {
+        info!("Moving actuator:  direction=FORWARD; gears={}", gears);
 
-                pos += 1;
-            }
+        pin_motor_plus.set_high();
+        Timer::after_millis(TIME_DISTANCE_PER_GEAR * ((gears as i64).abs() as u64)).await;
 
-            pin_motor_plus.set_high();
-            Timer::after_millis(50).await;
-            pin_motor_plus.set_low();
-            Timer::after_millis(50).await;
-        }
+        pin_motor_plus.set_low();
+        Timer::after_millis(50).await;
     } else {
-        info!("Moving actuator: direction=BACKWARD; amount={}", amount);
-        pin_motor_plus.set_low(); // Set the other pin to low. There can be only one!
+        info!("Moving actuator: direction=BACKWARD; gears={}", gears);
 
-        // FAKE: Simulate move by toggling the pin HIGH and LOW `amount` (mm) times.
-        let mut pos: i8 = 0; // Make sure to blink BOTH at completion of every position move.
-        for i in 0..=amount {
-            // FAKE: For every position, turn BOTH led on for a bit, to indicate position.
-            trace!("pos={}; i={}", pos, i);
-            if i % ACTUATOR_DISTANCE_BETWEEN_POSITIONS == 0 {
-                if pos != 0 {
-                    trace!("i % {}", ACTUATOR_DISTANCE_BETWEEN_POSITIONS);
-                    pin_motor_minus.set_high();
-                    pin_motor_plus.set_high();
-                    Timer::after_millis(100).await;
-                    pin_motor_minus.set_low();
-                    pin_motor_plus.set_low();
-                }
+        pin_motor_minus.set_high();
+        Timer::after_millis(TIME_DISTANCE_PER_GEAR * gears as u64).await;
 
-                pos += 1;
-            }
-
-            pin_motor_minus.set_high();
-            Timer::after_millis(50).await;
-            pin_motor_minus.set_low();
-            Timer::after_millis(50).await;
-        }
+        pin_motor_minus.set_low();
+        Timer::after_millis(50).await;
     }
 
     // TODO: Verify with the potentiometer on the actuator that we've actually moved it to the right position.
-    //       Documentation say "Actual resistance value may vary within the 0-10kΩ range based on stroke length".
+    let _actuator_pot_measurement = read_pot(pot_adc, pot_pin).await;
 }
 
 // Test actuator control. Move it backward 1mm, then forward 1mm.
@@ -82,13 +81,73 @@ pub async fn move_actuator(
 pub async fn test_actuator(
     pin_motor_plus: &mut Output<'static>,
     pin_motor_minus: &mut Output<'static>,
+    pot_adc: &mut Adc<'static, AdcAsync>,
+    pot_pin: &mut adc::Channel<'static>,
 ) -> bool {
     info!("Testing actuator control");
-    move_actuator(-1, pin_motor_plus, pin_motor_minus).await;
+
+    let position_1: i16 = read_pot(pot_adc, pot_pin).await;
+    move_actuator(-1, pin_motor_plus, pin_motor_minus, pot_adc, pot_pin).await;
+
+    let position_2: i16 = read_pot(pot_adc, pot_pin).await;
     Timer::after_millis(100).await;
-    move_actuator(1, pin_motor_plus, pin_motor_minus).await;
 
-    // TODO: How do we know the actuator test worked?
+    move_actuator(1, pin_motor_plus, pin_motor_minus, pot_adc, pot_pin).await;
+    let position_3: i16 = read_pot(pot_adc, pot_pin).await;
 
-    true
+    // Verify move - we only check that it HAVE moved, not with how much..
+    if (position_1 == position_3) && (position_1 != position_2) {
+        true
+    } else {
+        false
+    }
+}
+
+// Read the actuator potentiometer value.
+pub async fn read_pot(
+    pot_adc: &mut Adc<'static, AdcAsync>,
+    pot_pin: &mut adc::Channel<'static>,
+) -> i16 {
+    match pot_adc.read(pot_pin).await {
+        Ok(val) => val as i16,
+        Err(e) => {
+            error!("Failed to read actuator porentiometer value: {:?}", e);
+            0
+        }
+    }
+}
+
+// Find the current gear.
+pub async fn find_gear(
+    pot_adc: &mut Adc<'static, AdcAsync>,
+    pot_pin: &mut adc::Channel<'static>,
+    start: i16,
+) -> i8 {
+    // Get the actuator potentiometer value - ACTUAL position of the actuator.
+    // On mine, it's a value between 550Ω (fully retracted - 'P') and 4680Ω (fully extended - 'D').
+    // Manufacturer say between 0-10kΩ.
+    let pot = read_pot(pot_adc, pot_pin).await;
+
+    // Distance positions. Ok, so this is dumb, but it's clear. And will be clear to me,
+    // five years from now when/if I go back in here and needs to fix something!! :D :D.
+    let gear_p: i16 = start + (RESISTANCE_BETWEEN_POSITIONS * 0); // Button::P
+    let gear_r: i16 = start + (RESISTANCE_BETWEEN_POSITIONS * 1); // Button::R
+    let gear_n: i16 = start + (RESISTANCE_BETWEEN_POSITIONS * 2); // Button::N
+    let gear_d: i16 = start + (RESISTANCE_BETWEEN_POSITIONS * 3); // Button::D
+
+    // Figure out what gear is in by the actuator potentiometer value.
+    // The actual GEAR position (which is what we need!), depends on where the 'P' gear position is.
+    // Can't use `match`, because `runtime values cannot be referenced in patterns`!!
+    let gear: i16 = (pot - start).try_into().unwrap();
+    if (gear > (gear_p - POSITION_MATCH_RANGE)) && (gear < (gear_p + POSITION_MATCH_RANGE)) {
+        0 // Button::P
+    } else if (gear > (gear_r - POSITION_MATCH_RANGE)) && (gear < (gear_r + POSITION_MATCH_RANGE)) {
+        1 // Button::R
+    } else if (gear > (gear_n - POSITION_MATCH_RANGE)) && (gear < (gear_n + POSITION_MATCH_RANGE)) {
+        2 // Button::N
+    } else if (gear > (gear_d - POSITION_MATCH_RANGE)) && (gear < (gear_d + POSITION_MATCH_RANGE)) {
+        3 // Button::D
+    } else {
+        4 // Button::UNSET
+    }
 }
