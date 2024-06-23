c6fee3bec6317e3ef4b1e6b5c72007b2d884bf7f
Turbo Fredriksson <turbo@bayour.com>
2024-06-23 09:54:19 +0100

    Fix the math.
    Got confused by it not working, but think that was 'somewhere else'
    in the code. Works now..
    + Update code comments.
    + Improve pin setup.
    + Add some trace debugging.
    * Can't use `move_actuator()` in the `test_actuator()` function.
      It moves whole gears, but we only want to move it 1mm back and
      forth, so do the move manually.
    * Make the `read_pot()` private. Shouldn't be exposed in the public
      API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![no_std]
 
-use defmt::{error, info};
+use defmt::{error, info, trace};
 
 use embassy_rp::adc;
 use embassy_rp::adc::{
     Adc, AdcPin, Async as AdcAsync, Config as AdcConfig, InterruptHandler as ADCInterruptHandler,
 };
-use embassy_rp::gpio::{AnyPin, Level, Output, Pull};
+use embassy_rp::gpio::{AnyPin, Drive, Level, Output, Pull, SlewRate};
 use embassy_rp::interrupt::typelevel::{Binding, ADC_IRQ_FIFO};
 use embassy_rp::peripherals::ADC;
 use embassy_rp::Peripheral;
@@ -18,32 +18,34 @@ use core::marker::PhantomData;
 // Setup static values, measured on the actuator.
 
 // The resistance on the actuator at MIN and MAX throw is 550Ω and 4690Ω respectively.
-static RESISTANCE_MIN_THROW: i16 = 550;
-static RESISTANCE_MAX_THROW: i16 = 4690;
+// NOTE: The manufacturer say: 'Actual resistance value may vary within the 0-10kΩ range based on stroke length'.
+//       I have a 4"/35lbs throw actuator: https://www.progressiveautomations.com/products/linear-actuator-with-potentiometer?variant=18277322391619.
+static RESISTANCE_MIN_THROW: i16 = 550; // Ω
+static RESISTANCE_MAX_THROW: i16 = 4690; // Ω
 
 // The total throw from MIN to MAX is `100.60mm` (`3.9605"`).
-static TOTAL_THROW_DISTANCE: f32 = 100.60;
+static TOTAL_THROW_DISTANCE: f32 = 100.60; // mm
 
-// Moving the actuator between endpoints takes exactly (!) 7s@4-5V; 2s@12V.
-static TOTAL_THROW_TIME: u8 = 2;
+// Moving the actuator between endpoints takes exactly (!) 7s@4V; 2s@12V.
+static TOTAL_THROW_TIME: i16 = 2000; // ms
 
-// The distance between the different gears is 70mm.
-static DISTANCE_BETWEEN_POSITIONS: u8 = 70;
+// The distance between 'P' and 'D' is 70mm.
+static DISTANCE_BETWEEN_POSITIONS: f32 = (70 / 3) as f32; // mm
 
 // The allowed +/- value between MIN and MAX gear position is 2mm.
-static ALLOWED_GEAR_DIFFERENCE: u8 = 2;
+static ALLOWED_GEAR_DIFFERENCE: u8 = 2; // mm
 
 // =====
 // These are the actual information we're interested in.
 static TOTAL_THROW_RESISTANCE: i16 = RESISTANCE_MAX_THROW - RESISTANCE_MIN_THROW;
 static THROW_RESISTANCE_PER_1MM: i16 = TOTAL_THROW_RESISTANCE / TOTAL_THROW_DISTANCE as i16;
 static POSITION_MATCH_RANGE: i16 = THROW_RESISTANCE_PER_1MM * ALLOWED_GEAR_DIFFERENCE as i16;
-static THROW_TIME_PER_1MM: f32 = TOTAL_THROW_TIME as f32 / TOTAL_THROW_DISTANCE as f32;
 
 static RESISTANCE_BETWEEN_POSITIONS: i16 =
     THROW_RESISTANCE_PER_1MM * DISTANCE_BETWEEN_POSITIONS as i16;
-static TIME_DISTANCE_PER_GEAR: u64 =
-    (THROW_TIME_PER_1MM * DISTANCE_BETWEEN_POSITIONS as f32) as u64;
+
+static THROW_TIME_PER_1MM: f32 = (TOTAL_THROW_TIME as f32 / TOTAL_THROW_DISTANCE as f32); // * 1000.0;
+static THROW_TIME_PER_GEAR: u64 = (THROW_TIME_PER_1MM * DISTANCE_BETWEEN_POSITIONS as f32) as u64;
 
 // =====
 
@@ -55,7 +57,6 @@ pub struct Actuator<'l, A: AdcPin> {
     phantom: PhantomData<&'l A>,
 }
 
-// `let mut actuator = Actuator::new(p.PIN_27, p.PIN_28, p.PIN_26, p.ADC, Irqs);`
 impl<'l, A: AdcPin> Actuator<'l, A> {
     pub fn new(
         pin_motor_plus: AnyPin,
@@ -65,12 +66,25 @@ impl<'l, A: AdcPin> Actuator<'l, A> {
         irqs: impl Binding<ADC_IRQ_FIFO, ADCInterruptHandler>,
     ) -> Self {
         // Initialize the motor pins.
-        let actuator_motor_plus = Output::new(pin_motor_plus, Level::Low); // Actuator/Motor Relay (#1)
-        let actuator_motor_minus = Output::new(pin_motor_minus, Level::Low); // Actuator/Motor Relay (#2)
+        let mut actuator_motor_plus = Output::new(pin_motor_plus, Level::Low); // Actuator/Motor Relay (#1)
+        let mut actuator_motor_minus = Output::new(pin_motor_minus, Level::Low); // Actuator/Motor Relay (#2)
+
+        actuator_motor_plus.set_drive_strength(Drive::_4mA);
+        actuator_motor_plus.set_slew_rate(SlewRate::Slow);
+
+        actuator_motor_minus.set_drive_strength(Drive::_4mA);
+        actuator_motor_minus.set_slew_rate(SlewRate::Slow);
 
         // Initialize the potentiometer pin.
         let adc = Adc::new(adc, irqs, AdcConfig::default());
         let actuator_potentiometer = adc::Channel::new_pin(pot_pin, Pull::None); // Actuator/Potentiometer Brush
+
+        // => `Distance between gear positions=23.0mm; Throw time/mm=19.880716ms`.
+        trace!(
+            "Distance between gear positions={}mm; Throw time/mm={}ms",
+            DISTANCE_BETWEEN_POSITIONS,
+            THROW_TIME_PER_1MM
+        );
 
         Self {
             motor_plus: actuator_motor_plus,
@@ -89,11 +103,20 @@ impl<'l, A: AdcPin> Actuator<'l, A> {
         self.motor_plus.set_low();
         self.motor_minus.set_low();
 
+        // How long to keep the pin HIGH to move to the designated gear position.
+        let sleep = THROW_TIME_PER_GEAR * ((gears as i64).abs() as u64);
+        trace!(
+            "Move sleep='{}ms * {}gears = {}ms'",
+            THROW_TIME_PER_GEAR,
+            ((gears as i64).abs() as u64),
+            sleep
+        );
+
         if gears < 0 {
-            info!("Moving actuator:  direction=FORWARD; gears={}", gears);
+            info!("Moving actuator: direction=FORWARD; gears={}", gears);
 
             self.motor_plus.set_high();
-            Timer::after_millis(TIME_DISTANCE_PER_GEAR * ((gears as i64).abs() as u64)).await;
+            Timer::after_millis(sleep).await;
 
             self.motor_plus.set_low();
             Timer::after_millis(50).await;
@@ -101,7 +124,7 @@ impl<'l, A: AdcPin> Actuator<'l, A> {
             info!("Moving actuator: direction=BACKWARD; gears={}", gears);
 
             self.motor_minus.set_high();
-            Timer::after_millis(TIME_DISTANCE_PER_GEAR * gears as u64).await;
+            Timer::after_millis(sleep).await;
 
             self.motor_minus.set_low();
             Timer::after_millis(50).await;
@@ -113,19 +136,43 @@ impl<'l, A: AdcPin> Actuator<'l, A> {
 
     // Test actuator control. Move it backward 1mm, then forward 1mm.
     // This should be safe to do EVEN IF (!!) we're moving (for whatever reason).
+    // NOTE: Can't use `move_actuator()` here, that moves whole gears, not distances (in mm).
     pub async fn test_actuator(&mut self) -> bool {
         info!("Testing actuator control");
 
+        // Read start position (Ω).
         let position_1: i16 = self.read_pot().await;
-        self.move_actuator(-1).await;
+        trace!("Actuator test position (#1): {}", position_1);
 
+        // Move the actuator one mm BACKWARD.
+        info!(
+            "Moving actuator BACKWARD 1mm (sleep={}ms)",
+            THROW_TIME_PER_1MM
+        );
+        self.motor_plus.set_high();
+        Timer::after_millis(THROW_TIME_PER_1MM as u64).await;
+        self.motor_plus.set_low();
+
+        // Read position.
         let position_2: i16 = self.read_pot().await;
-        Timer::after_millis(100).await;
+        trace!("Actuator test position (#2): {}", position_2);
 
-        self.move_actuator(1).await;
+        // Move the actuator one mm FORWARD.
+        info!(
+            "Moving actuator FORWARD 1mm (sleep={}ms)",
+            THROW_TIME_PER_1MM
+        );
+        self.motor_minus.set_high();
+        Timer::after_millis(THROW_TIME_PER_1MM as u64).await;
+        self.motor_minus.set_low();
+
+        // Read end position. Should be the same as start position..
         let position_3: i16 = self.read_pot().await;
+        trace!("Position (#3): {}", position_3);
 
         // Verify move - we only check that it HAVE moved, not with how much..
+        // TODO: Although.. Because we're waiting `THROW_TIME_PER_1MM`, we should
+        //       have moved 1mm. As in, the difference should be `THROW_RESISTANCE_PER_1MM`.. ?
         if (position_1 == position_3) && (position_1 != position_2) {
             true
         } else {
@@ -134,7 +181,7 @@ impl<'l, A: AdcPin> Actuator<'l, A> {
     }
 
     // Read the actuator potentiometer value.
-    pub async fn read_pot(&mut self) -> i16 {
+    async fn read_pot(&mut self) -> i16 {
         match self.adc.read(&mut self.feedback).await {
             Ok(val) => val as i16,
             Err(e) => {
