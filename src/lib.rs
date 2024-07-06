e4db748eccc0c457acd2ae441457305d92f40961
Turbo Fredriksson <turbo@bayour.com>
2024-07-06 15:08:19 +0100

    Pre-calculate the resistance for the different drive modes.
    + After we've checked that the actuator works (in `test_actuator()`),
      we try to figure out what gear we're in. If we're in between gear
      modes, use a new `move_to_exact_position()` method to move it to an
      exact (resistance) position.
    + Add a third output pin - select +5V/+12V to the actuator - to control
      the speed at which it moves. Need to move it slower when we need
      to move it if we're in between gear modes.
    + Add a temporary second param to `change_gear_mode()`, so that we
      can fake the current gear (that we can read from the flash). This
      until we can get the actuator to move and work correctly.
    
    * Rewrite and simplify `find_gear()`. Use the new `check_gear_position()`
      to find exactly where the actuator is, instead of doing it here.
      We'll need that functionality in the new `move_to_exact_position()`
      as well.
    * Move functions around for cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use defmt::{debug, trace, Format};
+use defmt::{debug, error, trace, Format};
 
 use embassy_rp::adc::{Adc, AdcPin, Async, Channel, Config, InterruptHandler};
 use embassy_rp::gpio::{AnyPin, Drive, Level, Output, Pull, SlewRate};
@@ -28,6 +28,9 @@ static RESISTANCE_MAX_THROW: u16 = 1800; // Ω
 //   * 'D':  430Ω
 static RESISTANCE_PER_GEAR: u16 = 400;
 
+// Where the first gear mode is located (Ω from endpoint).
+static ACTUATOR_START_RESISTANCE: u16 = 30;
+
 // The total throw from MIN to MAX is `101.56mm` (`3.9984"`).
 static TOTAL_THROW_DISTANCE: f32 = 101.56; // mm
 
@@ -53,6 +56,13 @@ pub static THROW_TIME_PER_GEAR: u64 =
 static ALLOWED_GEAR_DIFFERENCE_RESISTANCE: u16 =
     (ALLOWED_GEAR_DIFFERENCE_DISTANCE as u16) * THROW_RESISTANCE_PER_1MM;
 
+// Pre-calculate the proper gear mode positions, based on the start position
+// (which is where the 'D' mode is - max pulled in).
+static GEAR_D: u16 = ACTUATOR_START_RESISTANCE + (RESISTANCE_PER_GEAR * 1); // Button::D
+static GEAR_N: u16 = ACTUATOR_START_RESISTANCE + (RESISTANCE_PER_GEAR * 2); // Button::N
+static GEAR_R: u16 = ACTUATOR_START_RESISTANCE + (RESISTANCE_PER_GEAR * 3); // Button::R
+static GEAR_P: u16 = ACTUATOR_START_RESISTANCE + (RESISTANCE_PER_GEAR * 4); // Button::P
+
 #[derive(Copy, Clone, Format, PartialEq)]
 #[repr(u8)]
 pub enum Direction {
@@ -65,6 +75,7 @@ pub enum Direction {
 pub struct Actuator<'l, PotPin: AdcPin> {
     motor_plus: Output<'l>,
     motor_minus: Output<'l>,
+    v_select: Output<'l>,
     feedback: Channel<'l>,
     adc: Adc<'l, Async>,
     phantom: PhantomData<&'l PotPin>,
@@ -74,6 +85,7 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
     pub fn new(
         pin_motor_plus: AnyPin,
         pin_motor_minus: AnyPin,
+        pin_volt_select: AnyPin,
         pot_pin: PotPin,
         adc: impl Peripheral<P = ADC> + 'l,
         irqs: impl Binding<ADC_IRQ_FIFO, InterruptHandler>,
@@ -81,6 +93,7 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
         // Initialize the motor pins.
         let mut actuator_motor_plus = Output::new(pin_motor_plus, Level::Low);
         let mut actuator_motor_minus = Output::new(pin_motor_minus, Level::Low);
+        let mut actuator_voltage_select = Output::new(pin_volt_select, Level::Low);
 
         // Initialize the potentiometer pin.
         let adc = Adc::new(adc, irqs, Config::default());
@@ -90,10 +103,12 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
         // https://wiki.purduesigbots.com/electronics/general/output-drive
         actuator_motor_plus.set_drive_strength(Drive::_2mA);
         actuator_motor_minus.set_drive_strength(Drive::_2mA);
+        actuator_voltage_select.set_drive_strength(Drive::_2mA);
 
         // https://wiki.purduesigbots.com/electronics/general/slew-rate
         actuator_motor_plus.set_slew_rate(SlewRate::Fast);
         actuator_motor_minus.set_slew_rate(SlewRate::Fast);
+        actuator_voltage_select.set_slew_rate(SlewRate::Fast);
 
         // Output some basic info we're using.
         trace!(
@@ -117,6 +132,7 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
         Self {
             motor_plus: actuator_motor_plus,
             motor_minus: actuator_motor_minus,
+            v_select: actuator_voltage_select,
             feedback: actuator_potentiometer,
             adc: adc,
             phantom: PhantomData,
@@ -142,6 +158,9 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
         self.move_actuator((THROW_TIME_PER_1MM as u64) * 2, Direction::Forward)
             .await;
 
+        // Give it 1/10s to settle.
+        Timer::after_millis(100).await;
+
         // Read position.
         let position_2: u16 = self.read_pot().await;
         trace!(
@@ -149,18 +168,18 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
             position_2
         );
 
-        // Give it 1/10s to settle.
-        Timer::after_millis(100).await;
-
         // Make sure the actuator moved.
-        if !self.verify_moved(position_1, position_2).await {
-            // Return test failure.
+        if !self.verify_moved(position_1, position_2) {
+            // Did not move - return test failure.
             return false;
         }
 
         // Move the actuator 2mm backward.
         self.move_actuator((THROW_TIME_PER_1MM as u64) * 2, Direction::Backward)
             .await;
+
+        // Give it 1/10s to settle.
+        Timer::after_millis(100).await;
 
         // Read end position. Should be the same as start position..
         let position_3: u16 = self.read_pot().await;
@@ -169,18 +188,69 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
             position_3
         );
 
+        // =====
         // Verify overall move.
-        // Should be NO - have NOT moved between #1 and #3! As in, returned to original position.
-        if !self.verify_moved(position_1, position_3).await {
-            // Return test success.
-            return true;
-        } else {
-            // Return test failure.
+        if self.verify_moved(position_1, position_3) {
+            // Have moved (it didn't return to original position) - return test failure.
             return false;
         }
+
+        // =====
+        // The actuator worked, find what gear we're in.
+        if self.find_gear().await == 4 {
+            // UNSET. As in, we're in between gears!
+            // NOTE: We know where the actuator is *now* (`position_3`).
+            //       What is the closest gear mode from here?
+            if position_3 < (GEAR_R + ((GEAR_R - GEAR_P) / 2)) {
+                // Move to 'P'.
+                trace!(
+                    "test_actuator(): position_3={} < {}",
+                    position_3,
+                    (GEAR_R + ((GEAR_R - GEAR_P) / 2))
+                );
+                self.move_to_exact_position(0).await; // Button::P
+            } else if position_3 < (GEAR_N + ((GEAR_N - GEAR_R) / 2)) {
+                // Move to 'R'
+                trace!(
+                    "test_actuator(): position_3={} < {}",
+                    position_3,
+                    (GEAR_N + ((GEAR_N - GEAR_R) / 2))
+                );
+                self.move_to_exact_position(1).await; // Button::R
+            } else if position_3 < (GEAR_D + ((GEAR_D - GEAR_N) / 2)) {
+                // Move to 'N'
+                trace!(
+                    "test_actuator(): position_3={} < {}",
+                    position_3,
+                    (GEAR_D + ((GEAR_D - GEAR_N) / 2))
+                );
+                self.move_to_exact_position(2).await; // Button::N
+            } else {
+                // Move to 'D'
+                trace!("test_actuator(): position_3={} (else)", position_3);
+                self.move_to_exact_position(3).await; // Button::D
+            }
+
+            return true; // For now, just return success.
+        }
+
+        return true;
     }
 
-    pub async fn change_gear_mode(&mut self, gears: i8) {
+    // Move the actuator to a specific gear mode position.
+    // TODO: Remove the `fake` param as soon as the actuator works as intended.
+    pub async fn change_gear_mode(&mut self, mode: u8, fake: i8) {
+        // Get the resistance feedback value from the actuator.
+        let current_gear = self.find_gear().await;
+        debug!(
+            "change_gear_mode(): Found current gear mode: {:?}",
+            current_gear
+        );
+        let current_gear = fake; // TODO: Remove when actuator work as intended.
+
+        // Calculate the amount of gears and direction to move.
+        let gears: i8 = current_gear - mode as i8;
+
         // How long to keep the pin HIGH to move to the designated gear position.
         let move_time = THROW_TIME_PER_GEAR * ((gears as i64).abs() as u64);
         trace!(
@@ -190,6 +260,7 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
             move_time
         );
 
+        // Move the actuator to the new gear mode position.
         if gears < 0 {
             self.move_actuator(move_time, Direction::Backward).await;
         } else {
@@ -197,58 +268,190 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
         }
     }
 
-    // Find the current gear.
-    pub async fn find_gear(&mut self, start: u16) -> i8 {
+    // ==================================================
+    // Private functions.
+    // ==================================================
+
+    // Read the actuator potentiometer and translate the resistance from it to a gear mode positon.
+    async fn find_gear(&mut self) -> i8 {
         // Get the actuator potentiometer value - ACTUAL position of the actuator.
-        // Manufacturer say between 0-10kΩ.
+        // Manufacturer say between 0-10kΩ, depending on throw distance.
         let pot = self.read_pot().await;
-        if pot < start {
+
+        // Sanity check, just to make sure we don't get a value that is LOWER than the first mode.
+        if pot < ACTUATOR_START_RESISTANCE {
             debug!("Can't find gear, no resonable value from pot - return 'Button::UNSET'");
-            return 4;
+            return 4; // Button::UNSET
         }
 
-        // Distance positions. Ok, so this is dumb, but it's clear. And will be clear to me,
-        // five years from now when/if I go back in here and needs to fix something!! :D :D.
-        let gear_d: u16 = start + (RESISTANCE_PER_GEAR * 1); // Button::D
-        let gear_n: u16 = start + (RESISTANCE_PER_GEAR * 2); // Button::N
-        let gear_r: u16 = start + (RESISTANCE_PER_GEAR * 3); // Button::R
-        let gear_p: u16 = start + (RESISTANCE_PER_GEAR * 4); // Button::P
-
-        // Figure out what gear is in by the actuator potentiometer value.
-        // The actual GEAR position (which is what we need!), depends on where the 'P' gear position is.
-        // Can't use `match`, because `runtime values cannot be referenced in patterns`!!
-        let gear: u16 = (pot - start).try_into().unwrap();
+        // Calculate what gear is in by the actual actuator potentiometer value.
+        let current_gear_mode: u16 = (pot - ACTUATOR_START_RESISTANCE).try_into().unwrap();
         trace!(
-            "find_gear(): gear={}; gear_p={}; gear_r={}; gear_n={}; gear_d={}",
-            gear,
-            gear_p,
-            gear_r,
-            gear_n,
-            gear_d
+            "find_gear(): gear={}; GEAR_P={}; GEAR_R={}; GEAR_N={}; GEAR_D={}",
+            current_gear_mode,
+            GEAR_P,
+            GEAR_R,
+            GEAR_N,
+            GEAR_D
         );
-        if (gear > (gear_p - ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-            && (gear < (gear_p + ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-        {
+
+        // Find the gear mode position, within a fault tolerance of the actuator
+        // potentiometer and reading (the Pico ADC isn't very precise).
+        if self.check_gear_position(current_gear_mode, GEAR_P) {
             0 // Button::P
-        } else if (gear > (gear_r - ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-            && (gear < (gear_r + ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-        {
+        } else if self.check_gear_position(current_gear_mode, GEAR_R) {
             1 // Button::R
-        } else if (gear > (gear_n - ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-            && (gear < (gear_n + ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-        {
+        } else if self.check_gear_position(current_gear_mode, GEAR_N) {
             2 // Button::N
-        } else if (gear > (gear_d - ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-            && (gear < (gear_d + ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
-        {
+        } else if self.check_gear_position(current_gear_mode, GEAR_D) {
             3 // Button::D
         } else {
             4 // Button::UNSET
         }
     }
 
-    // =====
+    // Check if after-move position is between pos1 max/min.
+    // Returns:
+    //   * TRUE  => Have moved.
+    //   * FALSE => Have not moved.
+    fn verify_moved(&mut self, before: u16, after: u16) -> bool {
+        // NOTE: We only check that it HAVE moved, not with how much.
+        // NOTE: Take a +-10Ω difference on the reading. The ADC in the Pico isn't very accurate.
+        trace!("verify_moved(before={}, after={})", before, after);
+        if before < 25 {
+            debug!(
+                "Can't verify move, no resonable value from pot - return 'FALSE' (Have NOT moved)"
+            );
+            return false;
+        }
+
+        let before_min = before - ALLOWED_GEAR_DIFFERENCE_RESISTANCE;
+        let before_max = before + ALLOWED_GEAR_DIFFERENCE_RESISTANCE;
+        trace!(
+            "verify_moved(): (after({}) > before_min({})) && (after({}) < before_max({}))",
+            after,
+            before_min,
+            after,
+            before_max
+        );
+
+        if (after > before_min) && (after < before_max) {
+            trace!("verify_moved(): FALSE (Have NOT moved)");
+            return false;
+        } else {
+            trace!("verify_moved(): TRUE (HAVE moved)");
+            return true;
+        }
+    }
+
+    // Simplify the check if a value falls within a certain tolerance.
+    fn check_gear_position(&mut self, check: u16, validate: u16) -> bool {
+        if (check > (validate - ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
+            && (check < (validate + ALLOWED_GEAR_DIFFERENCE_RESISTANCE))
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Given a gear mode position, move in small steps until the actuator potentiometer
+    // roughly matches (within `ALLOWED_GEAR_DIFFERENCE_RESISTANCE`Ω) that gear mode.
+    // TODO: Should we even have a specific position to move to, why not just go to 'P'?
+    // TODO: With a third relay, we can choose between +5V and +12V,
+    //       and switch to the +5V instead, which will move the actuator
+    //       slower.
+    async fn move_to_exact_position(&mut self, mode: u8) -> bool {
+        // Just define the variable..
+        let mut dest: u16 = 0;
+
+        // The way we calculate before calling this function, is that we're to far
+        // *behind* (backward) of the position we want to go, so we move *forward*.
+        let mut direction = Direction::Forward;
+
+        // Start with 1/10s.
+        let mut move_time: u64 = 100;
+
+        // Find out what resistance the potentiometer should read when we're done.
+        if mode == 0 {
+            // Button::P
+            dest = GEAR_P;
+        } else if mode == 1 {
+            // Button::R
+            dest = GEAR_R;
+        } else if mode == 2 {
+            // Button::N
+            dest = GEAR_N;
+        } else if mode == 3 {
+            // Button::D
+            dest = GEAR_D;
+        }
+        debug!(
+            "move_to_exact_position(mode={}): Wanted position: {}Ω",
+            mode, dest
+        );
+
+        // Keep moving the actuator until we've arrived at the destination.
+        loop {
+            // We read the potentiometer at the *end*, because we already know we're off..
+
+            // Move actuator.
+            if direction == Direction::Forward {
+                self.motor_plus.set_high();
+            } else {
+                self.motor_minus.set_high();
+            }
+            Timer::after_millis(move_time).await;
+
+            // Stop actuator. Simpler to just put both LOW, than figure out which one..
+            self.motor_plus.set_low();
+            self.motor_minus.set_low();
+
+            // Read the new position.
+            let position: u16 = self.read_pot().await;
+            trace!(
+                "move_to_exact_position(): new actuator potentiometer value: {}",
+                position
+            );
+
+            // Check if the new position is near the value of the gear mode we wanted.
+            if self.check_gear_position(position, dest) {
+                // We're done, we're at the desired gear mode.
+                // Within +/- `ALLOWED_GEAR_DIFFERENCE_RESISTANCE`Ω
+                self.v_select.set_low(); // Go back to using +12V for the actuator.
+                return true;
+            } else if position > dest {
+                // We moved too far, change the direction and lower the move time.
+                if direction == Direction::Forward {
+                    direction = Direction::Backward;
+                } else {
+                    direction = Direction::Forward;
+                }
+                move_time = move_time / 2 as u64;
+                self.v_select.set_high(); // Switch to +5V to make the actuator move slower.
+            } // else: keep moving.
+        }
+    }
+
+    // ==================================================
     // NOTE: These should really be private functions, but I need them to do the calibration.
+    // ==================================================
+
+    // Read the actuator potentiometer value.
+    // NOTE: Should probably get an average here.
+    //       https://rust-classes.com/chapter_embedded_pi_input
+    //       However, that takes forever! There's a really annoying delay before something
+    //       happens, so just take this value. However, it turns out it's within +/- 10Ω.
+    //       We can deal with that in the `check_gear_position()` method.
+    pub async fn read_pot(&mut self) -> u16 {
+        match self.adc.read(&mut self.feedback).await {
+            Ok(val) => val as u16,
+            Err(e) => {
+                error!("Failed to read actuator porentiometer value: {:?}", e);
+                0
+            }
+        }
+    }
 
     // Move the actuator - distance.
     pub async fn move_actuator(&mut self, distance: u64, direction: Direction) -> bool {
@@ -307,50 +510,6 @@ impl<'l, PotPin: AdcPin> Actuator<'l, PotPin> {
             actuator_pot_2
         );
 
-        return self.verify_moved(actuator_pot_1, actuator_pot_2).await;
-    }
-
-    // Check if after-move position is between pos1 max/min.
-    // Returns:
-    //   * TRUE  => Have moved.
-    //   * FALSE => Have not moved.
-    async fn verify_moved(&mut self, before: u16, after: u16) -> bool {
-        // NOTE: We only check that it HAVE moved, not with how much.
-        // NOTE: Take a +-10Ω difference on the reading. The ADC in the Pico isn't very accurate.
-        trace!("verify_moved(before={}, after={})", before, after);
-        if before < 25 {
-            debug!(
-                "Can't verify move, no resonable value from pot - return 'FALSE' (Have NOT moved)"
-            );
-            return false;
-        }
-
-        let before_min = before - ALLOWED_GEAR_DIFFERENCE_RESISTANCE;
-        let before_max = before + ALLOWED_GEAR_DIFFERENCE_RESISTANCE;
-        trace!(
-            "verify_moved(): (after({}) > before_min({})) && (after({}) < before_max({}))",
-            after,
-            before_min,
-            after,
-            before_max
-        );
-
-        if (after > before_min) && (after < before_max) {
-            trace!("verify_moved(): FALSE (Have NOT moved)");
-            return false;
-        } else {
-            trace!("verify_moved(): TRUE (HAVE moved)");
-            return true;
-        }
-    }
-
-    // Read the actuator potentiometer value.
-    // NOTE: Should probably get an average here.
-    //       https://rust-classes.com/chapter_embedded_pi_input
-    //       However, that takes forever! There's a really annoying delay before something
-    //       happens, so just take this value. However, it turns out it's within +/- 10Ω.
-    //       We can deal with that elsewhere..
-    pub async fn read_pot(&mut self) -> u16 {
-        return self.adc.read(&mut self.feedback).await.unwrap();
+        return self.verify_moved(actuator_pot_1, actuator_pot_2);
     }
 }
